7d93ef86f021b0743aab7b86d12704416a5586be
Tal Arbatov <talarbatov98@gmail.com>
2021-07-30 23:29:52 +0300

    fixed boilerplate

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// import configStore from './store';
+// import { Provider } from 'react-redux';
+
+// const store = configStore();
+
+ReactDOM.render(
+  
+    <App />
+  
+  ,document.getElementById('application'));
