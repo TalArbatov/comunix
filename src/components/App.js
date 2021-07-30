b8bcf93ecea2eba80dda8d8a2dc28ffdc415b540
Tal Arbatov <talarbatov98@gmail.com>
2021-07-30 23:27:31 +0300

    created basic react boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from './components/Wrapper';
+import { GlobalStyle } from './styled';
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      <div>hello world!</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Frutiger, "Frutiger Linotype", Univers, Calibri, "Gill Sans", "Gill Sans MT", "Myriad Pro", Myriad, "DejaVu Sans Condensed", "Liberation Sans", "Nimbus Sans L", Tahoma, Geneva, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    .event-card {
+      margin-bottom: 20px;
+      background-color: #fafafa;
+      width: 100%;
+      
+    }
+  }
+  
+`;
