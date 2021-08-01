a41fa899e3db6bf3bed087be288b1cca788edc24
Tal Arbatov <talarbatov98@gmail.com>
2021-08-01 17:12:13 +0300

    test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // TODO: integrate env variables
 const config = {
   clientId: '652fd53d0cc34dc2a950e39a95cc18b6',
-  redirectionUrl: 'https://comunix-assignment.herokuapp.com/'
+  redirectionUrl: 'https://comunix-assignment.herokuapp.com'
 };
 
 export default config;
